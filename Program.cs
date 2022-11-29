d34975ad45aaadd7be66b61c7813a957d1648cd0
Mowei <tso88206@gmail.com>
2022-11-29 15:44:50 +0800

    Update Program.cs
    開啟預設首頁

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿//https://weblog.west-wind.com/posts/2019/Jan/22/COM-Object-Access-and-dynamic-in-NET-Core-2x
+//https://weblog.west-wind.com/posts/2019/Jan/22/COM-Object-Access-and-dynamic-in-NET-Core-2x
 //https://github.com/RickStrahl/Westwind.Utilities
 using System.Reflection;
+using Microsoft.Win32;
+
+var startPage = "about:blank";
+RegistryKey registryKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main");
+if (registryKey.GetValue("Start Page") != null) { startPage = registryKey.GetValue("Start Page").ToString(); }
+
 var MemberAccess =
     BindingFlags.Public | BindingFlags.NonPublic |
     BindingFlags.Static | BindingFlags.Instance | BindingFlags.IgnoreCase;
@@ -9,4 +15,4 @@ var ie = Activator.CreateInstance(type);
 ie.GetType().InvokeMember("Visible", MemberAccess | BindingFlags.SetProperty, null, 
     ie, new object[1] { true });
 ie.GetType().InvokeMember("Navigate", MemberAccess | BindingFlags.InvokeMethod, null,
-    ie, new object[] { "about:blank" });
+    ie, new object[] { startPage });
